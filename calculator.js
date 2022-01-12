cea0cb7e195d24e93117989dbba732b8638f9a9d
Tomasz Wawrzyniak <tomasz.wawrzyniak@capgemini.com>
2022-01-12 16:49:22 +0100

    sugar

--- calculator.js
+++ calculator.js
@@ -1,27 +1,30 @@
 import { isOperator, trimEquaction } from "./helper.js";
-export function Calculator() {
-  this.result = "";
-  this.equation = "";
-  return {
-    appendNumber: (num) => {
-      this.result += num;
-    },
-    compute: () => {
-      this.equation = this.result;
-      this.result = Math.round(eval(this.equation) * 100) / 100;
-    },
-    clear: () => {
-      this.result = "";
-      this.equation = "";
-    },
-    applyOperator: (operator) => {
-      const last = this.result[this.result.length - 1];
-      if (!isOperator(last)) {
-        this.result += operator;
-      }
-    },
-    getResult: () => {
-      return { equation: this.equation, result: this.result };
-    },
-  };
+export class Calculator {
+    #result;
+    #equation;
+  constructor() {
+    this.#result = "";
+    this.#equation = "";
+  }
+
+  appendNumber(num) {
+    this.#result += num;
+  }
+  compute() {
+    this.#equation = this.#result;
+    this.#result = Math.round(eval(this.#equation) * 100) / 100;
+  }
+  clear() {
+    this.#result = "";
+    this.#equation = "";
+  }
+  applyOperator(operator) {
+    const last = this.#result[this.#result.length - 1];
+    if (!isOperator(last)) {
+      this.#result += operator;
+    }
+  }
+  getResult() {
+    return { equation: this.#equation, result: this.#result };
+  }
 }
